b3c264ade46fc88814a76de12c363083d3d6d829
ujjwal <ujjwalsangwan2001@gmail.com>
2023-04-13 09:36:16 +0530

    little bit edited

--- java_project.java
+++ java_project.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
 class Supplier {
-    private String name;
-    private int id;
-    private String date;
-    private String location;
-    private long contact;
+     String name;
+     int id;
+     String date;
+     String location;
+     long contact;
 
     public Supplier(String name, int id,long contact, String location) {
-        this.name = name;                              
-        this.id = id;
-        this.contact = contact;
-        this.location = location;
-        this.order_placed = new ArrayList<String>();
+        name = name;                              
+        id = id;
+        contact = contact;
+        location = location;
+        
     }
     public String getName() {
         return name;
@@ -30,11 +30,5 @@ class Supplier {
         return contact;
     }
 
-    public void Place_order(String order) {
-        order_placed.add(order);
-    }
-
-    public ArrayList<String> Order_placed() {
-        return order_placed;
-    }
+    
 }
